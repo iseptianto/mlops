45807516019755c5b4eb183b9433d3539f6f77f6
iseptianto <indraseptianto17@gmail.com>
2025-08-04 14:02:24 +0700

    update

--- tourism-api/app/app/app.py
+++ tourism-api/app/app/app.py
@@ -7,13 +7,14 @@ app = FastAPI()
 # Load tourism model
 model = mlflow.pyfunc.load_model(model_uri="models:/tourism-recommender-model@production")
 
+
 @app.get("/")
 def read_root():
     return {"api_status": "ok", "model_name": "tourism-recommender-model", "model_alias": "production"}
 
 @app.post("/predict")
-async def predict(request: Request):
-    input_data = await request.json()  # Expecting [{"user_id": "user_001"}]
-    input_df = pd.DataFrame(input_data)
-    predictions = model.predict(input_df)
+def predict(payload: List[Dict[str, str]]):
+    df = pd.DataFrame(payload)
+    predictions = model.predict(df)
     return {"recommendations": predictions}
+
