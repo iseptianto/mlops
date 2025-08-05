c6e72c826406db89673943efec4cadfb8c977b50
iseptianto <indraseptianto17@gmail.com>
2025-08-05 16:43:36 +0700

    bug

--- model/model_tourism/train_model.py
+++ model/model_tourism/train_model.py
@@ -5,11 +5,11 @@ import pickle
 mlflow.set_experiment("tourism_recommender")
 
 with mlflow.start_run():
-    mlflow.log_param("model_type", "content_similarity_recommender")
+    mlflow.log_param("model_type", "content_encoder_recommender")
 
     # Log each artifact (4 total)
     artifacts = {
-        "content_similarity.pkl": "content_similarity.pkl",
+        "content_encoder.pkl": "content_encoder.pkl",
         "place_encoder.pkl": "place_encoder.pkl",
         "prediction_matrix.pkl": "prediction_matrix.pkl",
         "user_encoder.pkl": "user_encoder.pkl"
